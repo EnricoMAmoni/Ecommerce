def2894832894d87994896f998588100f32bad6e
giorgia berretti <giorgiaberretti@MacBook-Pro-di-giorgia.local>
2022-09-02 16:53:30 +0200

    aggiunta leadership e modificate card

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ window.addEventListener('scroll', () => {
        navbarWrapper.classList.remove('scrolled')
     }
  })
+
+ AOS.init();
+
+ /* let stelline = document.querySelectorAll('.stelline');
+ stelline.forEach(stellina =>
+ stellina.addEventListener('mouseover', () => {
+   if(mouseover){
+   stellina.classList.remove('fa-regular')
+   stellina.classList.add('fa-solid')
+} else {
+   stellina.classList.add('fa-regular')
+   stellina.classList.remove('fa-solid')
+}
+ }))
+ */
+ let stelline = document.querySelectorAll('.stelline');
+ stelline.forEach(stellina =>
+   stellina.addEventListener('mouseover', () => 
+   stellina.innerHTML = `<i class="fa-solid fa-star"></i>`
+ ),
+
+ stelline.forEach(stellina =>
+   stellina.addEventListener('mouseleave', () =>
+   stellina.innerHTML = `<i class="fa-regular fa-star"></i>`
+   ))
+)
+
+/* stelline.forEach(stellina => {
+   if(stellina.addEventListener('mouseover') == true){
+      stellina.innerHTML = `<i class="fa-solid fa-star"></i>`
+   } else {
+      stellina.innerHTML = `<i class="fa-regular fa-star"></i>`
+   }
+   return stellina
+}) */
+
+
+
+
+
